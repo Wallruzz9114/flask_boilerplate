7f65d7f57519c541ace7f255b787e9ddbe1bba11
Jose Pinto <pintojose.benedicto2@gmail.com>
2020-01-16 03:29:09 -0700

    Added pytest setup

--- project/config.py
+++ project/config.py
@@ -4,6 +4,7 @@ import os
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "my_precious"
 
 
 class DevelopmentConfig(BaseConfig):
@@ -11,6 +12,7 @@ class DevelopmentConfig(BaseConfig):
 
 
 class TestingConfig(BaseConfig):
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
